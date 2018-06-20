df4c74fb9882896840fe445fcfc0cbdf6e61566d
hegglinmichael <mhegglinmichael@gmail.com>
2018-06-20 11:04:01 -0700

    changes

--- GrabEverythin.py
+++ GrabEverythin.py
@@ -5,30 +5,10 @@ def main():
 
     es = Elasticsearch()
 
-    # loops through a list of indices
-    # does access info in them
-    for index in es.indices.get("*"):
-        print("\n")
-        print("Info------")
-        print(index)
-        print("\n")
-
-    # more specific to a specific index in elasticsearch
-    print("testing specific index")
-    res = es.get(index="customer", doc_type="_doc", id=1)
-    print("this is res-------")
-    print(res)
-    print("\n")
-
-    # stuff below could be used in a look to gather all data
-    print("info held in index -----------------")
-    print("\n")
-    res = es.search(index="customer", body={"query": {"match_all": {}}})
-    for hit in res['hits']['hits']:
-        print(hit)
-        print("type: ", type(hit))
-        print("accessing element in the dict", hit["_source"]['name'])
-        print("\n")
+    for i in es.indices.get("*"):
+        res = es.get(index=i, body={"query": {"match_all": {}}})
+        for hit in res['hits']['hits']:
+            name = hit["_source"]['name']
 
 
 if __name__ == "__main__":
